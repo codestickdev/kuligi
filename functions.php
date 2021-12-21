496159a9314e3664299b77bf5a13f930ff7a1e31
codestickdev <piotrdevv@gmail.com>
2021-12-21 12:48:23 +0100

    ACF local

--- functions.php
+++ functions.php
@@ -170,6 +170,22 @@ function kuligi_scripts() {
 add_action( 'wp_enqueue_scripts', 'kuligi_scripts' );
 
 /**
+ * ACF local
+ */
+add_filter('acf/settings/save_json', 'my_acf_json_save_point');
+function my_acf_json_save_point( $path ) {
+    $path = get_stylesheet_directory() . '/acf-json';
+    return $path;
+}
+
+add_filter('acf/settings/load_json', 'my_acf_json_load_point');
+function my_acf_json_load_point( $paths ) {
+    unset($paths[0]);
+    $paths[] = get_stylesheet_directory() . '/acf-json';
+    return $paths;
+}
+
+/**
  * Custom scripts / styles
  */
 function kuligi_custom(){
