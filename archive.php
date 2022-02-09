9a6eafc77e1895f90a351bebfd7b2a61ecd5357a
codestickdev <piotrdevv@gmail.com>
2022-02-09 16:43:14 +0100

    droplabs info category

--- archive.php
+++ archive.php
@@ -46,6 +46,47 @@ $cat_id = $term->term_id;
     <?php endif; ?>
     
     <?php if(get_field('droplabs', 'term_' . $cat_id)): ?>
+        <section class="frontReassurance frontReassurance--single">
+        <div class="frontReassurance__wrap container">
+            <div class="frontReassurance__left">
+                <h2>Zaufaj profesjonalistom!</h2>
+                <p>Nasza firma pracuje na swoją reputację blisko 15 lat.<br/>Nie składamy obietnic bez pokrycia.</p>
+                <div class="usp">
+                    <div class="usp__icon">
+                        <img src="<?php echo get_template_directory_uri() . '/images/frontpage/reassurance_01.svg'; ?>"/>
+                        <p>Super<br/>opinie</p>
+                    </div>
+                    <div class="usp__icon">
+                        <img src="<?php echo get_template_directory_uri() . '/images/frontpage/reassurance_02.svg'; ?>"/>
+                        <p>Gwarancja jakości<br/>obsługi</p>
+                    </div>
+                    <div class="usp__icon">
+                        <img src="<?php echo get_template_directory_uri() . '/images/frontpage/reassurance_03.svg'; ?>"/>
+                        <p>Lata<br/>doświadczenia</p>
+                    </div>
+                </div>
+            </div>
+            <div class="frontReassurance__right">
+                <p>Bilety na nasz kulig szybko się wyprzedają. 
+                Rezerwując ONLINE masz gwarancję dostępności i najniższej ceny. Wybierz miejscowość, ilość osób, termin, ewentualne świadczenia dodatkowe, uzupełnij swoje dane i dokonaj płatności.</p>
+                <p>Proste, szybie i bezpiecznie!</p>
+                <p>Jesteśmy niezależni od warunków pogodowych. Kuligi zielone i białe odbywają się przez cały rok od wiosny do zimy. W razie braku śniegu organizujemy kuligi na bryczkach lub wozach drabiniastych z kołami. Każdy z naszych zaprzęgów jest zadaszony i chroni kuligowiczów przed śniegiem, wiatrem lub deszczem. Na każdym znajdują się ciepłe koce do okrycia.</p>
+            </div>
+        </div>
+    </section>
+    <section class="frontAbout frontAbout--single">
+        <?php include get_template_directory() . '/template-parts/_aboutBadges.php'; ?>
+    </section>
+    <section class="singleInfo">
+        <div class="singleInfo__heading">
+            <h2>Jak zarezerwować imprezę w naszym systemie rezerwacji imprez? Nic prostszego! </h2>
+        </div>
+        <div class="singleInfo__content">
+            <div class="content container">
+                <p>Klikasz w odpowiedni banerek, wybierasz odpowiednią ilość biletów normalnych (ewentualnie ulgowych w niektórych opcjach), przechodzisz do koszyka zakupów klikając w zielony koszyk, wybierasz datę i godzinę imprezy, klikasz DALEJ, jeśli są to wybierasz opcje dodatkowe do kuligu (np. napad zbójnicki, kapela góralska, gawędziarz, DJ Janosik itp.), klikasz DALEJ podajesz swoje dane takie jak imię i nazwisko, numer telefonu oraz mail (TELEFON, DANE I MAIL MUSZĄ BYĆ POPRAWNE ABY SYSTEM PRZYJĄŁ REZERWACJĘ - JAK PODASZ ZŁEGO MAILA, POTWIERDZENIE REZERWACJI NIE SPŁYNIE CI NA WSKAZANEGO MAILA - DLATEGO PROSIMY O DOKŁADNE SPRAWDZENIE DANYCH), możesz ponadto poprosić nas o wystawienie faktury wpisując w rubrykę wszystkie dane Twojej firmy z NIPem a my wyślemy fakturę na Twojego maila. Dalej trzeba KLIKNĄĆ: Zapoznałem się i akceptuję Regulamin sprzedaży, Politykę prywatności oraz Regulamin Kuligi w Beskidach a system przekieruje Cię automatycznie do płatności BlueCash. Można za kulig zapłacić BLIKIEM lub z dowolnego banku a także kartą kredytową. Po zapłaceniu od razu na wskazany przez Ciebie adres mailowy przychodzi potwierdzenie rezerwacji z całym programem imprezy i informacjami technicznymi. Rezerwacji nie trzeba drukować na imprezę, gdyż nasi pracownicy mają listy uczestników imprezy. Życzymy udanych rezerwacji i wspaniałej zabawy! TO TAKIE PROSTE :)))</p>
+            </div>
+        </div>
+    </section>
 	<section class="categoryDroplabs">
 		<div class="categoryDroplabs__wrap">
 			<?php echo get_field('droplabs', 'term_' . $cat_id); ?>
