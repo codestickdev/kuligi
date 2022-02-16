29ae8581fc0e697c912878bd4de6442c02b43a7c
codestickdev <piotrdevv@gmail.com>
2022-02-16 16:38:03 +0100

    404 fix

--- 404.php
+++ 404.php
@@ -13,9 +13,11 @@ get_header();
 	<main id="primary" class="site-main">
 
 		<section class="error-404 not-found">
-			<h1>404</h1>
-			<p>Strona o wskazanym adresie nie istnieje</p>
-			<a href="<?php echo get_home_url(); ?>" class="btn"><span>Strona główna</span></a>
+			<div class="wrap">
+				<h1>404</h1>
+				<p>Strona o wskazanym adresie nie istnieje</p>
+				<a href="<?php echo get_home_url(); ?>" class="btn"><span>Strona główna</span></a>
+			</div>
 		</section><!-- .error-404 -->
 
 	</main><!-- #main -->
